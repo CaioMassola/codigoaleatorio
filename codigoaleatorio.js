9e570ab2ed0d7bc9229761d54831bf3c04bd9f19
Caio Massola <47333514+CaioMassola@users.noreply.github.com>
2022-07-01 11:47:22 -0300

    Update codigoaleatorio.js

--- codigoaleatorio.js
+++ codigoaleatorio.js
@@ -2,24 +2,22 @@
 //Gerar codigo aleatorio
   
 function randomCodigo(params, valor) {
-      var random = '';
+  var random = '';
   var valores  = valor;
     for (var i = 0; i < params; i++) {
         random += valores.charAt(Math.floor(Math.random() * valores.length));
     }
-    return random;
+  
+  return random;
 }
 
 function gerarCodigo() {
+  
   var uppercase = randomCodigo(2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-
   var numeros = randomCodigo(4, '0123456789')
-
   var simbolo = randomCodigo(1, '@#$%&')
-
   var aux_uppercase = randomCodigo(2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
   var lowercase = randomCodigo(2,'abcdefghijklmnopqrstuvwxyz')
-
   var codigo = uppercase.concat(numeros,simbolo,aux_uppercase, lowercase)
 
   return codigo
